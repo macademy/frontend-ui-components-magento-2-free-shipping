0211ac3d41ce6cff059a2f7d73313696d1ff05d7
Mark Shust <mark@shust.com>
2022-11-18 15:24:16 -0500

    Subscribe to Knockout observables

--- view/frontend/web/js/free-shipping-banner.js
+++ view/frontend/web/js/free-shipping-banner.js
@@ -28,6 +28,12 @@ define([
                     self.subtotal = parseFloat(cart().subtotalAmount);
                 }
             });
+
+            cart.subscribe(function(cart) {
+                if (!_.isEmpty(cart) && !_.isUndefined(cart.subtotalAmount)) {
+                    self.subtotal = parseFloat(cart.subtotalAmount);
+                }
+            });
         },
         formatCurrency: function(value) {
             return '$' + value.toFixed(2);
