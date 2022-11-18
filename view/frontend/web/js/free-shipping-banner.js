63e56b523d074dac6a0ad77f954247ce6c952110
Mark Shust <mark@shust.com>
2022-11-18 15:21:52 -0500

    Use ViewModel functions for separation of concerns

--- view/frontend/web/js/free-shipping-banner.js
+++ view/frontend/web/js/free-shipping-banner.js
@@ -7,12 +7,16 @@ define([
 
     return Component.extend({
         defaults: {
+            subtotal: 33.00,
             template: 'Macademy_FreeShippingPromo/free-shipping-banner'
         },
         initialize: function() {
             this._super();
 
             console.log(this.message);
+        },
+        formatCurrency: function(value) {
+            return '$' + value.toFixed(2);
         }
     });
 });
