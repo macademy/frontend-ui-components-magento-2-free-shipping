1c67622d31c1ae1a613abe969712e3bf2890eb53
Mark Shust <mark@shust.com>
2022-11-18 14:55:25 -0500

    Create the most basic UI Component

---
+++ view/frontend/web/js/free-shipping-banner.js
@@ -0,0 +1,11 @@
+define([
+    'uiComponent'
+], function(
+    Component
+) {
+    'use strict';
+
+    console.log('Free Shipping Banner UI Component has been loaded');
+
+    return Component;
+});
